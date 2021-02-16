089188751c17bb50ee5ac46f7a4116ffb9c53ab2
romulofpaiva <romulofpaiva@gmail.com>
2021-02-16 16:46:54 -0300

    Starting the project

---
+++ src/main/java/br/com/casadocodigo/conf/AppWebConfiguration.java
@@ -0,0 +1,25 @@
+package br.com.casadocodigo.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import br.com.casadocodigo.controllers.HomeController;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = { HomeController.class })
+public class AppWebConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
